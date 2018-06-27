6c57708dd8e9b56a91eb1a8524aa35063541e62c
linchengzhang <lcz_1996@foxmail.com>
2018-06-27 23:13:18 +0800

    roman to integer

---
+++ roman_to_integer.py
@@ -0,0 +1,21 @@
+"""
+Example 1:
+
+Input: "III"
+Output: 3
+"""
+
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        maps = {"I": 1, "V": 5, "X": 10, "L": 50, "C":100, "D": 500, "M": 1000}
+        ans = 0
+        for i in range(len(s)):
+            if i > 0 and maps[s[i]] > maps[s[i - 1]]:
+                ans += maps[s[i]] - 2 * maps[s[i - 1]]
+            else:
+                ans += maps[s[i]]
+        return ans
