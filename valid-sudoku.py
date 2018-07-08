7440e426739973e3b372a4a7598403ca91840051
linchengzhang <lcz_1996@foxmail.com>
2018-07-08 08:00:32 +0800

    valid-sudoku

---
+++ valid-sudoku.py
@@ -0,0 +1,62 @@
+"""
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Example 1:
+
+Input:
+[
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: true
+
+"""
+
+class Solution:
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        for i in range(9):
+            if not self.isValid([board[i][j] for j in range(9)]) \
+                or not self.isValid([board[j][i] for j in range(9)]):
+                return False        
+
+        for i in range(3):
+            for j in range(3):
+                if not self.isValid([board[m][n] for m in range(3 * i, 3 * i + 3) for n in range(3 * j, 3 * j + 3)]):
+                    print(2)
+                    return False
+
+        return True
+
+
+    def isValid(self, ls):
+        ls = list(filter(lambda x: x != '.', ls))
+        return len(set(ls)) == len(ls)
+
+board = [
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+
+print(Solution().isValidSudoku(board))
