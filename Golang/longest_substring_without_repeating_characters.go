5f35d35f67622699dff29c55d09e2d0c685942d7
linchengzhang <lcz_1996@foxmail.com>
2019-02-18 22:13:59 +0800

    golang

---
+++ Golang/longest_substring_without_repeating_characters.go
@@ -0,0 +1,19 @@
+func max(a , b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func lengthOfLongestSubstring(s string) int {
+	lookup := make(map[string] int)
+	j, ans := 0, 0
+	for i := 0; i < len(s); i++ {
+		if _, ok := lookup[string(s[i])]; ok {
+			j = max(lookup[string(s[i])], j)
+		}
+		ans = max(ans, i - j + 1)
+		lookup[string(s[i])] = i + 1
+	}
+	return ans
+}
